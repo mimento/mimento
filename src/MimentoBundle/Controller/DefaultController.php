2518fc6a258b336456edd8a272ea6845680cd303
mimento <contact@nhuynh.fr>
2016-03-09 14:19:24 +0100

    1

---
+++ src/MimentoBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MimentoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MimentoBundle:Default:index.html.twig');
+    }
+}
---
+++ src/MimentoBundle/MimentoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MimentoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MimentoBundle extends Bundle
+{
+}
